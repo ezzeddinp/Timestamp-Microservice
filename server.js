23826b3d633906b6f2cb00e059467c9041f4e0fc
Mike Hatter <mike@mikehatter.com>
2020-09-03 16:43:46 -0400

    Defaulted the date variable to the current timestamp, added a check for whether the date parameter was passed to the route, and returned the unix/utc time in json

--- server.js
+++ server.js
@@ -16,7 +16,19 @@ app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
 app.get('/api/hello', (req, res) => res.json({ greeting: 'hello API' }));
 
 // Handle returning a timestamp
-app.get('/api/timestamp/:date?', (req, res) => res.json({ message: 'Not yet implemented' }));
+app.get('/api/timestamp/:date?', (req, res) => {
+    // Store our date response. This will default to the current datetime
+    let date = new Date();
+
+    // Check if the optional date parameter was provided
+    if (req.params.date) {
+        // TODO: Handle the date parameter
+        res.json({ message: 'Date parameter passed', date: req.params.date });
+    }
+
+    // Return the unix and UTC time
+    res.json({ unix: date.getTime(), utc: date.toUTCString() });
+});
 
 // Create a listener to handle requests
 const listener = app.listen(process.env.PORT, () => console.log('Your app is listening on port ' + listener.address().port));
